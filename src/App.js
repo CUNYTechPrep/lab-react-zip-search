61dd032b257b010ee123cde1389e97b3ce475656
dwang <dwang.ccny@gmail.com>
2022-10-10 16:50:11 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import React from "react";
+
+import React, { useState } from "react";
 import "./App.css";
 
-function City(props) {
-  return <div>This is the City component</div>;
+function ZipSearchField(props) {
+  const [zip,setZip] = useState("");
+  return (
+    <form action="" classname="form">
+      <input
+        className="zip"
+        value={zip}
+        placeholder="xxxxx"
+        type="text"
+        name="zip"
+        id="zip"
+        onChange={(event)=> {
+          const {value} = event.target;
+          setZip(value.replace(/[^\d{5}]$/,"").substr(0,5)) //expression to allow only #s & substr will only allow 5 in the form
+        }}
+        />
+    </form>
+  
+  )
 }
 
-function ZipSearchField(props) {
-  return <div>This is the ZipSearchField component</div>;
+function ResultModule(props){
+  return(
+    <div >
+      <h2 id="h2">{props.zip.City}, {props.zip.State}</h2>
+        <ul>
+          <div id="li">
+             <li>State: {props.zip.State}</li>
+             <li>Location: {props.zip.Location}</li>
+             <li>Estimated Population: {props.zip.EstimatedPopulation}</li>
+             <li>Total Wages: {props.zip.TotalWages}</li>
+          </div>
+        </ul>
+      </div>
+  )
 }
 
 function App() {
+  //Obtain Data
+  const initialSettings = {city:"", state:""}
+  const [data,setData] = useState([]);
+  const [zipCode, setZipCode] = useState("");
+
+  const obtainInfo = () => {
+    fetch('https://ctp-zip-api.herokuapp.com/zip/' + zipCode , {
+      'mode' : 'cors',
+      headers: {
+        'Content-Type' : 'application/json',
+      }
+    })
+    .then((response) => response.json())
+    .then((responseData) => setData(responseData))
+  }
+
   return (
     <div className="App">
-      <div className="App-header">
-        <h1>Zip Code Search</h1>
-      </div>
-      <div className="mx-auto" style={{ maxWidth: 400 }}>
-        <ZipSearchField />
-        <div>
-          <City />
-          <City />
+      <h1>Zip Code Search</h1>      
+      <div style={{marginTop: 20}}>
+      <label id= "zipCode" htmlFor="zip">Zip Code</label>
+        <div className="input">
+          <input type="text" onChange={(e) => setZipCode(e.target.value)}></input>
+        </div>
+        <button onClick={() => obtainInfo(zipCode)}>Search</button>
+    
+        <div className="resultModule">
+          {data.map((zip) => (
+            <ResultModule zip={zip}></ResultModule>
+        ))}
         </div>
       </div>
     </div>
