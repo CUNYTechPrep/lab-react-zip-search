c97e84559382b1f40f430c8f11c957efe669ddb9
Mohamed Elsayed <78699419+M-Elsayed9@users.noreply.github.com>
2023-10-06 08:16:20 -0400

    Feat- Search Cities by Zipcode

--- src/App.js
+++ src/App.js
@@ -1,26 +1,102 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
-function City(props) {
-  return <div>This is the City component</div>;
+function City({ cityName, state, latitude, longitude, population, wages }) {
+  return (
+    <div className="card mb-5">
+      <div className="card-header">
+        {cityName}, {state}
+      </div>
+      <div className="card-body" style={{ width: 400 }}>
+        <ul>
+          <li>State: {state}</li>
+          <li>
+            Location: ({latitude}, {longitude})
+          </li>
+          <li>Population (estimated): {population}</li>
+          <li>Total Wages: {wages}</li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
-function ZipSearchField(props) {
-  return <div>This is the ZipSearchField component</div>;
+function ZipSearchField({ onSearch, zipCode }) {
+  const handleInput = (e) => {
+    onSearch(e.target.value);
+  };
+
+  return (
+    <div>
+      <label htmlFor="zip">Zip Code:</label>
+      <div style={{ margin: 10 }}></div>
+      <input
+        id="zip"
+        type="text"
+        onChange={handleInput}
+        value={zipCode}
+      ></input>
+    </div>
+  );
 }
 
 function App() {
+  const [zipCode, setZipCode] = useState("");
+  const [cities, setCities] = useState([]);
+  const [noResult, setNoResult] = useState(false);
+
+  const handleSearch = (newZipCode) => {
+    setZipCode(newZipCode);
+
+    if (newZipCode.length === 5) {
+      let request = `https://ctp-zip-code-api.onrender.com/zip/${newZipCode}`;
+      fetch(request)
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+          return response.json();
+        })
+        .then((data) => {
+          if (data.length === 0) {
+            setNoResult(true);
+            setCities([]);
+          } else {
+            setNoResult(false);
+            setCities(data);
+          }
+        })
+        .catch(() => {
+          setNoResult(true);
+          setCities([]);
+        });
+    }
+  };
+
   return (
     <div className="App">
       <div className="App-header">
         <h1>Zip Code Search</h1>
       </div>
+      <div style={{ margin: 50 }}></div>
       <div className="mx-auto" style={{ maxWidth: 400 }}>
-        <ZipSearchField />
-        <div>
-          <City />
-          <City />
-        </div>
+        <ZipSearchField onSearch={handleSearch} zipCode={zipCode} />
+        <div style={{ margin: 50 }}></div>
+        {noResult ? (
+          <div>No results found</div>
+        ) : (
+          cities.map((city, index) => (
+            <City
+              key={index}
+              cityName={city.City}
+              state={city.State}
+              latitude={city.Lat}
+              longitude={city.Long}
+              population={city.EstimatedPopulation}
+              wages={city.TotalWages}
+            />
+          ))
+        )}
       </div>
     </div>
   );
