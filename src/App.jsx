dbf9f5ba0e35c5b8ffd57c5139df77d5eadbd6c1
JNikolo <125705821+JNikolo@users.noreply.github.com>
2024-09-16 02:06:36 -0400

    feat: Cities card rendering with API fetched data

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,63 @@ import { useState } from "react";
 import "./App.css";
 
 function City(props) {
-  return <div>This is the City component</div>;
+  return <div>
+    <h2>{props.data.City}</h2>
+    <ul>
+      <li>State: {props.data.State}</li>
+      <li>Location: ({props.data.Lat}, {props.data.Long})</li>
+      <li>Population (estimated): {props.data.EstimatedPopulation}</li>
+      <li>Total Wages: {props.data.TotalWages}</li>
+    </ul>
+  </div>;
 }
 
 function ZipSearchField(props) {
-  return <div>This is the ZipSearchField component</div>;
+
+  const zipChanged = async (zip) => {
+    if (zip.length === 5) {
+      try{
+        const response = await fetch(`https://ctp-zip-code-api.onrender.com/zip/${zip}`);
+        const data = await response.json();
+        console.log(data);
+        props.onChange(data);
+      } catch (error) {
+        console.error(error);
+        props.onChange([]);
+      }
+    }
+    else{
+      props.onChange([]);
+    }
+  }
+
+  return <div>
+          <label>Zip Code:</label>
+          <input 
+            onChange={e => zipChanged(e.target.value)}
+            type="text"
+            placeholder="Try 10016"
+          />
+        </div>;
 }
 
 function App() {
+  const [cities, setCities] = useState([]);
   return (
     <div className="App">
       <div className="App-header">
         <h1>Zip Code Search</h1>
       </div>
       <div className="mx-auto" style={{ maxWidth: 400 }}>
-        <ZipSearchField />
+        <ZipSearchField cities={cities} onChange={setCities} />
         <div>
-          <City />
-          <City />
+          {cities && cities.length > 0 ? (
+            cities.map((city, index) => (
+              <City key={index} data={city} />
+            ))
+          ) : (
+            <div>No Results</div>
+          )}
         </div>
       </div>
     </div>
