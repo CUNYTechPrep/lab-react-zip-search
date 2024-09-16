dc24815f8a98226c4e1f471cc379888f72bff964
Kevin Orta <kevino6532@hstat.org>
2024-09-16 16:10:13 -0400

    Added zip field and sending API zip request

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function City(props) {
@@ -6,17 +6,44 @@ function City(props) {
 }
 
 function ZipSearchField(props) {
-  return <div>This is the ZipSearchField component</div>;
+  return (
+    <>
+      <label for="zip-code">Zip Code</label>
+      <input
+        id="zip-code"
+        type="text"
+        onChange={(e) => {
+          if (e.target.value.length === 5) {
+            props.onZipChange(e.target.value);
+          }
+        }}
+      ></input>
+    </>
+  );
 }
 
 function App() {
+  const [zipCode, setZipCode] = useState(0);
+  const [zipData, setZipData] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(
+        "https://ctp-zip-code-api.onrender.com/zip/" + zipCode
+      );
+      const data = await response.json();
+      setZipData(data);
+    };
+    fetchData();
+    console.log(zipData);
+    console.log(zipCode, "- Has changed");
+  }, [zipCode]);
   return (
     <div className="App">
       <div className="App-header">
         <h1>Zip Code Search</h1>
       </div>
       <div className="mx-auto" style={{ maxWidth: 400 }}>
-        <ZipSearchField />
+        <ZipSearchField onZipChange={setZipCode} />
         <div>
           <City />
           <City />
