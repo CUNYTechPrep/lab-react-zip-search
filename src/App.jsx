6b959db08b7037aea8256b7feffb3dd31a988886
Ahnaf Ahmed <61428434+AhnafAhmed13@users.noreply.github.com>
2024-09-18 13:33:03 -0400

    Add handler with states

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,37 @@ function City(props) {
 }
 
 function ZipSearchField(props) {
-  return <div>This is the ZipSearchField component</div>;
+  const [inputValue, setInputValue] = useState("");
+  async function handleInputChange(event) {
+    setInputValue(event.target.value);
+    // Check if valid zipcode
+    if (!inputValue.match(/[0-9]{5}/)) return;
+    console.log("Valid zipcode");
+    const baseURL = "https://ctp-zip-code-api.onrender.com";
+    const endpoint = `/zip/${inputValue}`;
+    try {
+      let response = await fetch(baseURL + endpoint);
+      let body = await response.json();
+      console.log("Success", body);
+    } catch (error) {
+      console.error("Error", error);
+    }
+  }
+
+  return (
+    <div>
+      <form method="GET">
+        <label htmlFor="zipcode">Zip Code: </label>
+        <input
+          type="text"
+          name="zipcode"
+          id="zipcode"
+          placeholder="Try 10016"
+          onChange={handleInputChange}
+        />
+      </form>
+    </div>
+  );
 }
 
 function App() {
